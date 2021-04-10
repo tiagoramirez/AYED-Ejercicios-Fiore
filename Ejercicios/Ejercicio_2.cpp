f94575446f185807062102107c09e645263c0fcb
Tiago <tirama2001@gmail.com>
2021-04-10 16:54:58 -0300

    terminado el ejercicio 2

---
+++ Ejercicios/Ejercicio_2.cpp
@@ -0,0 +1,24 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int l1, l2, l3;
+    cout << "Ingresa un lado: ";
+    cin >> l1;
+    cout << "Ingresa otro lado: ";
+    cin >> l2;
+    cout << "Ingresa otro lado: ";
+    cin >> l3;
+    if (l1 == l2 and l1 == l3) {
+        cout << "Es un triangulo equilatero";
+    }
+    else{
+        if (l1 != l2 and l1 != l3 and l2 != l3) {
+            cout << "Es un triangulo escaleno";
+        }
+        else {
+            cout << "Es un triangulo isosceles";
+        }
+    }
+    return 0;
+}*/
--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
+    int n, m;
+    cout << "Ingresa M: ";
+    cin >> m;
+    cout<< "Ingresa N: ";
+    cin >> n;
+    for(int i=0;)
     return 0;
-}
+} 
