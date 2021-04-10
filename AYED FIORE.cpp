dc0326c0f11f54e5129f60ca7db38da3ee3cae82
Tiago <tirama2001@gmail.com>
2021-04-10 00:30:12 -0300

    Haciendo ej 1

--- AYED FIORE.cpp
+++ AYED FIORE.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!\n";
+    int i, num, cont0 = 0, contPos = 0, sumPos = 0, sumNeg = 0;
+    double promPos = 0;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    for (i = 1; i < 10; i++) {
+        if (num == 0) {
+            cont0++;
+        }
+        if (num > 0) {
+            sumPos += num;
+            contPos++;
+        }
+        if (num < 0) {
+            sumNeg += num;
+        }
+        cout << "Ingrese un numero: ";
+        cin >> num;
+    }
+    promPos = sumPos / (double)contPos;
+
+    cout << "Cantidad de 0: " << cont0 << endl;
+    cout << "Promedio positivos: " << promPos << endl;
+    cout << "Sumatoria negativos: " << sumNeg << endl;
 }
