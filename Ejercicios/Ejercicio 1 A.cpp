a81cbc02abbf92953225916732a4368704f5aba1
Tiago Notebook <tirama2001@gmail.com>
2021-04-10 01:23:09 -0300

    Ejercicio 1A hecho

---
+++ Ejercicios/Ejercicio 1 A.cpp
@@ -0,0 +1,26 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int i, num, cont0 = 0, contPos = 0, sumPos = 0, sumNeg = 0;
+    double promPos = 0;
+    for (i = 0; i < 167; i++) {
+        cout << "Ingrese un numero: ";
+        cin >> num;
+        if (num == 0) {
+            cont0++;
+        }
+        if (num > 0) {
+            sumPos += num;
+            contPos++;
+        }
+        if (num < 0) {
+            sumNeg += num;
+        }
+    }
+    promPos = sumPos / (double)contPos;
+    cout << "Cantidad de 0: " << cont0 << endl;
+    cout << "Promedio positivos: " << promPos << endl;
+    cout << "Sumatoria negativos: " << sumNeg << endl;
+}
+*/
--- main.cpp
+++ main.cpp
@@ -2,27 +2,5 @@
 using namespace std;
 
 int main(){
-    int i, num, cont0 = 0, contPos = 0, sumPos = 0, sumNeg = 0;
-    double promPos = 0;
-    cout << "Ingrese un numero: ";
-    cin >> num;
-    for (i = 1; i < 10; i++) {
-        if (num == 0) {
-            cont0++;
-        }
-        if (num > 0) {
-            sumPos += num;
-            contPos++;
-        }
-        if (num < 0) {
-            sumNeg += num;
-        }
-        cout << "Ingrese un numero: ";
-        cin >> num;
-    }
-    promPos = sumPos / (double)contPos;
-
-    cout << "Cantidad de 0: " << cont0 << endl;
-    cout << "Promedio positivos: " << promPos << endl;
-    cout << "Sumatoria negativos: " << sumNeg << endl;
+    return 0;
 }
