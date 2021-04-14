9368549c66e6b7dbf3f3c5c2279bce0fd16ef392
Tiago Notebook <tirama2001@gmail.com>
2021-04-13 23:07:15 -0300

    Terminado Ejercicio 6

---
+++ Ejercicios/Ejercicio_5.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int num, i=0,mult=1;
+    cout << "Ingresa un numero: ";
+    cin >> num;
+    while (i<num){
+        if (3*mult%5!=0){
+            cout << 3*mult<<endl;
+            i++;
+        }
+        mult++;
+    }
+    return 0;
+}*/
---
+++ Ejercicios/Ejercicio_6.cpp
@@ -0,0 +1,29 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int sueldo, res1=0, res2=0, res3=0, res4=0;
+    cout<<"Ingresa un sueldo";
+    cin >> sueldo;
+    while (sueldo!=0){
+        if(sueldo<520){
+            res1++;
+        }
+        if(sueldo>=520 and sueldo <780){
+            res2++;
+        }
+        if(sueldo>=780 and sueldo<999){
+            res3++;
+        }
+        if(sueldo>=999){
+            res4++;
+        }
+        cout<<"Ingresa otro sueldo";
+        cin >> sueldo;
+    }
+    cout<<"Empleados que ganan menos de 520: "<<res1<<endl;
+    cout<<"Empleados que ganan igual o mas de 520 y menos de 780: "<<res2<<endl;
+    cout<<"Empleados que ganan igual o mas de 780 y menos de 999:"<<res3<<endl;
+    cout<<"Empleados que ganan mas de 999: "<<res4<<endl;
+    return 0;
+}*/
