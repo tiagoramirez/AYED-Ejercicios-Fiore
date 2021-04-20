ce59a2bb1efe20a9e86dc92c834cfe2910c6c777
Tiago Notebook <tirama2001@gmail.com>
2021-04-20 20:17:54 -0300

    Agregado ejercicio de la clase 20/04/2021

---
+++ Ejercicios/Ejercicio_de_clase_20-4-2021.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int fecha,dia,mes,anio;
+    cout<<"Ingresa una fecha: ";
+    cin>>fecha;
+
+    dia=fecha/1000000;
+    mes=(fecha%1000000)/10000;
+    anio=fecha%10000;
+    
+    cout<<dia<<"/"<<mes<<"/"<<anio;
+    
+    return 0;
+} */
