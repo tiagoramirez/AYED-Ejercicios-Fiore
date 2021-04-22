67e61c433f8306d53eec65ce53ebbc611331a6f3
Tiago Notebook <tirama2001@gmail.com>
2021-04-21 23:38:12 -0300

    Terminado ejercicio 9

---
+++ Ejercicios/Ejercicio_9.cpp
@@ -0,0 +1,70 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    int valor1,valor2,valor3;
+    int max,med,min;
+    cout <<"Ingrese el valor 1: ";
+    cin >> valor1;
+    cout <<"Ingrese el valor 2: ";
+    cin >> valor2;
+    cout <<"Ingrese el valor 3: ";
+    cin >> valor3;
+    
+    while(valor1!=0 or valor2!=0 or valor3!=0){
+        if(valor1==valor2 and valor3==0){  //Este if lo puse por un bug que tenia(si ingresaba dos numeros iguales y un 0 no funcionaba. Ej>45-45-0)
+            max=valor1;
+            med=valor2;
+            min=valor3;
+        }
+        else{
+            if(valor1>valor2 and valor1>valor3){
+                max=valor1;
+                if(valor2>valor3){
+                    med=valor2;
+                    min=valor3;
+                }
+                else{
+                    med=valor3;
+                    min=valor2;
+                }
+            }
+            else{
+                if(valor2>valor1 and valor2>valor3){
+                    max=valor2;
+                    if(valor1>valor3){
+                        med=valor1;
+                        min=valor3;
+                    }
+                    else{
+                        med=valor3;
+                        min=valor1;
+                    }
+                }
+                else{
+                    max=valor3;
+                    if(valor2>valor1){
+                        med=valor2;
+                        min=valor1;
+                    }
+                    else{
+                        med=valor1;
+                        min=valor2;
+                    }
+                }
+            }
+        }
+        cout<<"#############################"<<endl;
+        cout<<min<<"-"<<med<<"-"<<max<<endl;
+        cout<<"#############################"<<endl;
+        cout<< "Ingresa otro valor 1: ";
+        cin >> valor1;
+        cout<< "Ingresa otro valor 2: ";
+        cin >> valor2;
+        cout<< "Ingresa otro valor 3: ";
+        cin >> valor3;
+    }
+    cout<<"Termino el programa.";
+    
+    return 0;
+}*/
--- main.cpp
+++ main.cpp
@@ -2,43 +2,5 @@
 using namespace std;
 
 int main(){
-    unsigned int boleto;
-    unsigned int num1,num2,num3,num4,num5;
-    unsigned int res1=0,res2=0,res3=0,res4=0;
-    cout<< "Ingresa un boleto: ";
-    cin>>boleto;
-    while(boleto!=0){
-        num1=boleto/10000;
-        num2=(boleto%10000)/1000;
-        num3=(boleto%1000)/100;
-        num4=(boleto%100)/10;
-        num5=boleto%10;
-        if(num1==num2 and num2==num3 and num3==num4 and num4==num5){
-            res1++;
-        }
-        else{
-            if(num1==num2 and num2==num4 and num4==num5){
-                res2++;
-            }
-            else{
-                if((num2==num3 and num2==num4 and num1==num5) or (num1==num3 and num1==num5 and num2==num4)){
-                    res3++;
-                }
-                else{
-                    if(num1==num5 and num2==num4){
-                        res4++;
-                    }
-                }
-            }
-        }
-        cout<<"Ingresa otro boleto: ";
-        cin >> boleto;
-    }
-    cout<<"####################################"<<endl;
-    cout<< "Boletos quintuples: "<<res1<<endl;
-    cout<< "Boletos cuadruples: "<<res2<<endl;
-    cout<< "Boletos triples: "<<res3<<endl;
-    cout<< "Boletos dobles: "<<res4<<endl;
-
     return 0;
-} 
+}
