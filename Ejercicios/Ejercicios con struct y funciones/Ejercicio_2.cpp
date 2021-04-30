3957e4b98d1529bfe24acc07be7ab5f15cd3b1c0
Tiago Notebook <tirama2001@gmail.com>
2021-04-30 19:38:53 -0300

    Terminado ejercicio 2 (struct y funciones

--- Ejercicios/Ejercicios con struct y funciones/Ejercicio_2.cpp
+++ Ejercicios/Ejercicios con struct y funciones/Ejercicio_2.cpp
@@ -6,7 +6,19 @@ int main(){
     Triangulo t;
     t = trianguloCreate();
     while(!esTrianguloNulo(t)){
-        
+        if (cantLadosIguales(t)==3){
+            cout<<"Es un triangulo equilatero."<<endl;
+        }
+        else{
+            if(cantLadosIguales(t)==2){
+                cout<<"Es un triangulo isosceles."<<endl;
+            }
+            else{
+                cout<<"Es un triangulo escaleno"<<endl;
+            }
+        }
+        t=trianguloCreate();
     }
+    cout<<"El programa termino...";
     return 0;
 }
