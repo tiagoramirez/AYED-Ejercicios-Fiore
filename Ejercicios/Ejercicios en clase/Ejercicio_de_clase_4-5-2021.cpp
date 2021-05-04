68883efcb23b50ee4ebc0f43111c0a409b953799
Tiago Notebook <tirama2001@gmail.com>
2021-05-04 19:29:19 -0300

    Agregado ejercicio 1 extra

---
+++ Ejercicios/Ejercicios en clase/Ejercicio_de_clase_4-5-2021.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+//El usuario ingresa numeros y finaliza ingresando el 999.
+//Mostrar cantidad de 0, promedio de positivos y suma de negativos.
+
+int main(){
+    int num, cont0 = 0, contPos = 0, sumPos = 0, sumNeg = 0;
+    double promPos = 0;
+
+    cout<<"Ingresa un numero: ";
+    cin>>num;
+
+    while (num!=999){
+        if (num == 0) {
+            cont0++;
+        }
+        else{
+            if (num > 0) {
+                sumPos += num;
+                contPos++;
+            }
+            else{
+                sumNeg += num;
+            }
+        }
+
+        cout<<"Ingresa otro numero: ";
+        cin>>num;
+    }
+
+    promPos = sumPos / (double)contPos;
+    cout << "Cantidad de 0: " << cont0 << endl;
+    cout << "Promedio positivos: " << promPos << endl;
+    cout << "Sumatoria negativos: " << sumNeg << endl;
+    return 0;
+}
