7ca5cfa566914803d3bb676401091d71bae41e18
Tiago Notebook <tirama2001@gmail.com>
2021-05-11 20:03:18 -0300

    Ejercicio del 11/5/2021 agregado

---
+++ Ejercicios/Ejercicios en clase/Ejercicio_de_clase_11-5-2021.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//Ejercicio 12 modificado
+//Ingresado un conjunto de valores terminado con 0, calcular su maximo
+//y la cantidad de veces que se repite
+//Ejemplo:
+//nros: 1 5 7 7 7 15 4 15 15 3 9 7 15 2
+//max=15
+//repeticiones=4
+
+int main(){
+    int valor, max=0,contRepeticiones=1;
+    bool esPrimerValor=true;
+
+    cout<<"Ingresa un valor: ";
+    cin >>valor;
+
+
+    while(valor!=0){
+        if(valor==max){
+            contRepeticiones++;
+        }
+        
+        if(esPrimerValor==true or valor>max){
+            esPrimerValor=false;
+            max=valor;
+            contRepeticiones=1;
+        }
+
+        cout<<"Ingresa otro valor: ";
+        cin >>valor;
+    }
+
+    cout<<"Maximo: "<<max<<endl;
+    cout<<"Repeticiones: "<<contRepeticiones<<endl;
+
+    return 0;
+}
